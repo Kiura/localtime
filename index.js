ff60512a9b96307a0be1f3626b1edc1d859bdd2d
Kiura Magomadov <m.k.x@mail.ru>
2019-09-04 01:28:53 +0200

    fix

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ bot.command(['listtimezones', 'listtimezones@localtime_bot'], async (ctx) => {
 	for (let [i, tz] of Object.entries(currenttzs)) {
 		message += `${tz.name}: ${tz.offsetStr}
 `
-		if (i > 3) {
+		if (i > 7) {
 			return ctx.reply(`${message}...`)		
 		}
 	}
